0eaa8902b2278043f16e5a409b2b74cbcab30b7a
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2021-01-01 21:55:13 +0800

    Done

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from 'react'
 
 const Title = () => {
   return (
     <div className="title">
       <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h2>Window of Life</h2>
+      <p>“ Get busy living or get busy dying.” </p>
+      <p>
+        <strong> Stephen King</strong>
+      </p>
     </div>
   )
 }
 
-export default Title;
+export default Title
